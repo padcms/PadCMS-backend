c8faa3b72e846a15cec84cf7d1e5c4ae854c3954
vladimir-doroshenko <vovabox@gmail.com>
2012-09-21 15:17:35 +0300

    Issue #46160: AM_Task_Manager implemented as daemon

--- application/include/AM/Task/Manager.php
+++ application/include/AM/Task/Manager.php
@@ -43,6 +43,26 @@ class AM_Task_Manager implements AM_Task_Manager_Interface
 {
     /** @var Zend_Log **/
     protected $_oLogger = null; /**< @type Zend_Log */
+    /** @var Zend_Config **/
+    protected $_oConfig = null; /**< @type Zend_Config */
+    /** @var integer **/
+    protected $_iProcessPid = null; /**< @type integer */
+    /** @var boolean **/
+    protected $_bStopDaemon = false; /**< @type boolean */
+
+    /**
+     * Get config
+     *
+     * @return Zend_Config
+     */
+    public function getConfig()
+    {
+        if (is_null($this->_oConfig)) {
+            $this->_oConfig = Zend_Registry::get('config');
+        }
+
+        return $this->_oConfig;
+    }
 
     /**
      * Get logger
@@ -71,45 +91,178 @@ class AM_Task_Manager implements AM_Task_Manager_Interface
     }
 
     /**
+     *  Get path of the pid file
+     * @return string
+     */
+    protected function _getPidFilePath()
+    {
+        $sPidFilePath = $this->getConfig()->temp->base . DIRECTORY_SEPARATOR . 'padcms_manager.pid';
+
+        return $sPidFilePath;
+    }
+
+    /**
+     * Saves the pid of the process
+     * @return AM_Task_Manager
+     */
+    protected function _createPidFile()
+    {
+        $sPidFilePath = $this->_getPidFilePath();
+        file_put_contents($sPidFilePath, getmypid());
+
+        return $this;
+    }
+
+    /**
+     * Removes the task manager daemon
+     * @return AM_Task_Manager
+     * @throws AM_Task_Manager_Exception
+     */
+    protected function _removePidFile()
+    {
+        if (!AM_Tools_Standard::getInstance()->unlink($this->_getPidFilePath())) {
+            //Can't unlink pid file
+            throw new AM_Task_Manager_Exception('Can\'t delete pidfile');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Check the pid file:
+     *  if file exists, get the pid from file and find the process with this pid
+     * @return boolean
+     * @throws AM_Task_Manager_Exception
+     */
+    protected function _isDemonized()
+    {
+        $sPidFilePath = $this->_getPidFilePath();
+
+        if (AM_Tools_Standard::getInstance()->is_file($sPidFilePath)) {
+            $iPid = file_get_contents($sPidFilePath);
+            //checking for the presence of the process
+            if (posix_kill($iPid, 0)) {
+                //the daemon already run
+
+                return true;
+            } else {
+                //pid file exists, but there isn't process
+                $this->_removePidFile();
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Habdler for unix sygnals
+     * @param int $iSignalNumber
+     */
+    public function signalHandler($iSignalNumber)
+    {
+        switch($iSignalNumber) {
+            case SIGQUIT:
+            case SIGTERM:
+                $this->_bStopDaemon = true;
+                $this->getLogger()->debug('SIGTERM catched. Stopping the daemon');
+                break;
+            default:
+                $this->getLogger()->debug(sprintf('Signal %d catched', $iSignalNumber));
+        }
+    }
+
+    /**
+     * Demonize the process
+     */
+    protected function _demonize()
+    {
+        if ($this->_isDemonized()) {
+            $this->getLogger()->debug('Process already demonized');
+            exit;
+        }
+
+        $this->_iProcessPid = pcntl_fork();
+
+        if (-1 == $this->_iProcessPid) {
+            throw new AM_Task_Manager_Exception('Can\'t demonize');
+        } else if ($this->_iProcessPid) {
+            //Killing the root process
+            exit;
+        }
+
+        $this->_createPidFile();
+        //Make the current process a session leader
+        posix_setsid();
+
+        $this->getLogger()->debug('Process demonized');
+    }
+
+    public function _bootstrap()
+    {
+        $oApplication = new Zend_Application(
+                        'daemon_' . APPLICATION_ENV,
+                        APPLICATION_PATH . '/configs/application.ini'
+        );
+
+        $oApplication->bootstrap()
+                ->run();
+    }
+
+    /**
      * Get new task and run it
      * @return void
      */
     public function run()
     {
-        $oTask = AM_Model_Db_Table_Abstract::factory('task')
-                ->findOneBy(array('status' => AM_Task_Worker_Abstract::STATUS_NEW));
-        /* @var $oTask AM_Model_Db_Task */
-
-        if (is_null($oTask)) {
-            $this->getLogger()->info('There are no tasks to execute');
-            return;
-        }
-
         try {
-            $this->getLogger()->debug('Running task '
-                    . sprintf('#%s with type #%s', $oTask->id, $oTask->task_type_id));
-
-            $oWorker = $oTask->getWorker();
-            /* @var $oWorker AM_Task_Worker_Abstract */
-
-            $this->getLogger()->debug('Task worker is '
-                    . sprintf('%s', get_class($oWorker)));
-
-            $oWorker->run();
-
-            $this->getLogger()->debug(sprintf('Finishing task #%s', $oTask->id));
-
-            $oWorker->finish();
-
-            $this->getLogger()->debug(sprintf('Task #%s is finished', $oTask->id));
-
-        } catch (Exception $e) {
-            $this->getLogger()->crit(sprintf('Task #%s has an error', $oTask->id));
-            $this->getLogger()->crit($e);
-
-            $oWorker->error($e);
-            return;
+            $this->_demonize();
+            $this->_bootstrap();
+        } catch (Exception $oException) {
+            $this->getLogger()->crit($oException->getMessage());
         }
+
+        declare(ticks=1);
+        pcntl_signal(SIGTERM, array($this, 'signalHandler'));
+        pcntl_signal(SIGQUIT, array($this, 'signalHandler'));
+
+        while (!$this->_bStopDaemon) {
+            sleep(5);
+
+            $oTask = AM_Model_Db_Table_Abstract::factory('task')
+                    ->findOneBy(array('status' => AM_Task_Worker_Abstract::STATUS_NEW));
+            /* @var $oTask AM_Model_Db_Task */
+
+            if (is_null($oTask)) {
+                $this->getLogger()->debug('There are no tasks to execute');
+                continue;
+            }
+
+            try {
+                $this->getLogger()->debug(sprintf('Running task #%s with type #%s', $oTask->id, $oTask->task_type_id));
+
+                $oWorker = $oTask->getWorker();
+                /* @var $oWorker AM_Task_Worker_Abstract */
+
+                $this->getLogger()->debug(sprintf('Task worker is  %s', get_class($oWorker)));
+
+                $oWorker->run();
+
+                $this->getLogger()->debug(sprintf('Finishing task #%s', $oTask->id));
+
+                $oWorker->finish();
+
+                $this->getLogger()->debug(sprintf('Task #%s is finished', $oTask->id));
+            } catch (Exception $oException) {
+                $this->getLogger()->crit(sprintf('Task #%s has an error', $oTask->id));
+                $this->getLogger()->crit($oException);
+
+                $oWorker->error($oException);
+                continue;
+            }
+        }
+
+        $this->_removePidFile();
+        $this->getLogger()->debug('Daemon stopped!');
     }
 
     public function error()
