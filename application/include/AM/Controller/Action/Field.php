549b8bdea5a4e125a2fe56143cf8cc5e96444465
vladimir-doroshenko <vovabox@gmail.com>
2012-10-15 15:54:23 -0400

    Fix: return Content-Type: text/plain in AM_Controller_Action_Field::upload actions - IE tries to download file when application/json header given

--- application/include/AM/Controller/Action/Field.php
+++ application/include/AM/Controller/Action/Field.php
@@ -250,7 +250,14 @@ class AM_Controller_Action_Field extends AM_Controller_Action
             $aMessage["message"]      = $this->__('Error. Can\'t upload file. ') . $this->__($e->getMessage());
         }
 
-        return $this->getHelper('Json')->sendJson($aMessage);
+        $aData = $this->getHelper('Json')->direct($aMessage, false);
+
+        $oResponse = $this->getResponse();
+        $oResponse->setHeader('Content-Type', 'text/plain', true);
+        $oResponse->setBody($aData);
+
+        $oResponse->sendResponse();
+        exit;
     }
 
     /**
@@ -318,7 +325,14 @@ class AM_Controller_Action_Field extends AM_Controller_Action
             $aMessage["errorMessage"] = $e->getMessage();
         }
 
-        return $this->getHelper('Json')->sendJson($aMessage);
+        $aData = $this->getHelper('Json')->direct($aMessage, false);
+
+        $oResponse = $this->getResponse();
+        $oResponse->setHeader('Content-Type', 'text/plain', true);
+        $oResponse->setBody($aData);
+
+        $oResponse->sendResponse();
+        exit;
     }
 
     /**
