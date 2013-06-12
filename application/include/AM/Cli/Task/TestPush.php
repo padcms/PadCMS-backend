8eb9a3f5fe97513acd53bdc5776092b7e2f271f1
Pavel Prischepa <pprischepa@adyax.com>
2013-06-12 20:30:10 +0700

    Implemented send test push message functionality. (cherry picked from commit 29e4194)

---
+++ application/include/AM/Cli/Task/TestPush.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Send test push message
+ * @ingroup AM_Cli
+ */
+class AM_Cli_Task_TestPush extends AM_Cli_Task_Abstract
+{
+
+    protected function _configure()
+    {
+      $this->addOption('issue', 'i', '=i', 'Issue id');
+      $this->addOption('token', 't', '=s', 'Device token');
+      $this->addOption('message', 'm', '=s', 'Message text');
+    }
+
+    public function execute()
+    {
+      $iIssueId  = intval($this->_getOption('token'));
+      $sToken   = $this->_getOption('token');
+      $sMessage = $this->_getOption('message');
+
+      $oTaskPlanner = new AM_Task_Worker_AppleNotification_Planner();
+      $oTaskPlanner->setOptions(array('issue_id' => $iIssueId,
+                                      'message'  => $sMessage,
+                                      'badge'    => 1,
+                                      'token'    => $sToken))
+                   ->create();
+    }
+}
