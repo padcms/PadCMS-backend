6f71b6b6f15f79641f09151be3c3c31ae5720170
Pavel Prischepa <pprischepa@adyax.com>
2013-06-20 18:13:17 +0700

    Small fixe.

--- application/include/AM/View/Helper/Field.php
+++ application/include/AM/View/Helper/Field.php
@@ -114,7 +114,7 @@ class AM_View_Helper_Field extends AM_View_Helper_Abstract
                 $aElementView['smallUri'] = AM_Tools::getIconForNonImageFile($sResourceFileName);
             }
 
-            $aElementView['originUri'] = '/content/element/' . AM_Tools_String::generatePathFromId(intval($oElement->id), '/') . '/' . $sResourceFileName;
+            $aElementView['originUri'] = '/content/element/' . AM_Tools_String::generatePathFromId(intval($oElement->id), '/') . "/$sResourceKeyName.{$aFileInfo['extension']}";
         }
 
         return $aElementView;
--- application/smarty/templates/includes/field/body.tpl
+++ application/smarty/templates/includes/field/body.tpl
@@ -37,7 +37,7 @@
 
             {if isset($field.element.originUri)}
               <div class="actions">
-                  <a class="action-2" target="_blank" href="{$field.element.originUri}" title="{'Download'|translate}"></a>
+                  <a class="action-2" target="_blank" href="{$field.element.originUri}" title="{'Download original file'|translate}"></a>
               </div>
             {/if}
 
