11ee6dac4072410e042153faae5cd935824e0465
Pavel Prischepa <pprischepa@adyax.com>
2013-09-30 21:24:42 +0700

    Small fixes

--- application/include/AM/Cli/Task/TestPush.php
+++ application/include/AM/Cli/Task/TestPush.php
@@ -5,20 +5,24 @@
  */
 class AM_Cli_Task_TestPush extends AM_Cli_Task_Abstract
 {
+  protected function _configure()
+  {
+    $this->addOption('issue', 'i', '=i', 'Issue id');
+    $this->addOption('token', 't', '=s', 'Device token');
+    $this->addOption('message', 'm', '=s', 'Message text');
+  }
 
-    protected function _configure()
-    {
-        $this->addOption('issue', 'i', '=i', 'Issue id');
-        $this->addOption('message', 'm', '=s', 'Message text');
-    }
+  public function execute()
+  {
+    $iIssueId  = intval($this->_getOption('token'));
+    $sToken   = $this->_getOption('token');
+    $sMessage = $this->_getOption('message');
 
-    public function execute()
-    {
-        $iIssueId  = intval($this->_getOption('issue'));
-        $sMessage = $this->_getOption('message');
-
-        AM_Task_Worker_Notification_Planner_Abstract::createTask(array('issue_id' => $iIssueId,
-                                                                       'message'  => $sMessage,
-                                                                       'badge'    => 1));
-    }
+    $oTaskPlanner = new AM_Task_Worker_AppleNotification_Planner();
+    $oTaskPlanner->setOptions(array('issue_id' => $iIssueId,
+                                    'message'  => $sMessage,
+                                    'badge'    => 1,
+                                    'token'    => $sToken))
+                 ->create();
+  }
 }
